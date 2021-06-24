59a0b807fb3d86e453f0a78f3e5cf418f085360a
Dana Brooks <dana.brooks@atos.net>
2021-06-24 16:56:20 -0400

    Update Package.swift for 0.3.0

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "RealtimeSDK",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "RealtimeSDK",
@@ -11,13 +11,13 @@ let package = Package(
     ],
     dependencies: [ 
                    .package(url: "https://github.com/phanikavi/apollo-ios", .upToNextMinor(from: "0.43.2")), 
-                   .package(url: "https://github.com/statsel/WebRTC", .upToNextMinor(from: "89.0.0")), 
+                   .package(url: "https://github.com/phanikavi/WebRTC", .upToNextMinor(from: "90.0.0")),
                    ],
     targets: [
         .binaryTarget(
             name: "RealtimeSDK",
-            url: "https://github.com/danabrooks/RealtimeSDK/releases/download/0.2.0/RealtimeSDK-0.2.0.xcframework.zip",
-            checksum: "b0a5f2963e82b237a759d11661e7222eb11fb454b6330f3c885d9e9ea3b60ad8"
+            url: "https://github.com/danabrooks/RealtimeSDK/releases/download/0.3.0/RealtimeSDK-0.3.0.xcframework.zip",
+            checksum: "1d741f1c63d94af4ff80805a64ada3155278e6e61561922cb6aa626bca5afca6"
         ),
     ]
 )
