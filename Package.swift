fa0fe58feaab6ed677a8e1a587ae3ffeef2f4b2a
Dana Brooks <dana.brooks@atos.net>
2021-06-28 15:39:50 -0400

    Update Package.swift for 0.4.1

--- Package.swift
+++ Package.swift
@@ -10,14 +10,14 @@ let package = Package(
             targets: ["RealtimeSDK"]),
     ],
     dependencies: [ 
-                   .package(url: "https://github.com/phanikavi/apollo-ios", .upToNextMinor(from: "0.43.2")), 
-                   .package(url: "https://github.com/phanikavi/WebRTC", .upToNextMinor(from: "90.0.0")),
+                   .package(url: "https://github.com/ATOS-VIRTUAL-CARE/apollo-ios", .branch("graphql-transport-ws")), 
+                   .package(url: "https://github.com/ATOS-VIRTUAL-CARE/webrtc-ios", .upToNextMinor(from: "90.0.0")),
                    ],
     targets: [
         .binaryTarget(
             name: "RealtimeSDK",
-            url: "https://github.com/danabrooks/RealtimeSDK/releases/download/0.4.0/RealtimeSDK-0.4.0.xcframework.zip",
-            checksum: "bac7b85e63c62be9d7e6fb32497595429ace2882b9624e30a71d90c8cbbcab8d"
+            url: "https://github.com/ATOS-VIRTUAL-CARE/realtime-sdk-ios/releases/download/0.4.1/RealtimeSDK-0.4.1.xcframework.zip",
+            checksum: "e64aac4f7d8f0c97c6cecf5eed0ebbfa480871a9b5ca8ec58753cf1f00ebc2ea"
         ),
     ]
 )
