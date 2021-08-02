b88dbc196316cb4dce3a9606f87034f61cf6dc9a
Dana Brooks <dana.brooks@atos.net>
2021-08-02 16:27:26 -0400

    Update Package.swift for 0.5.6

--- Package.swift
+++ Package.swift
@@ -10,14 +10,14 @@ let package = Package(
             targets: ["VcsRealtimeSdk"]),
     ],
     dependencies: [ 
-                   .package(url: "https://github.com/ATOS-VIRTUAL-CARE/apollo-ios", .branch("graphql-transport-ws")), 
+                   .package(url: "https://github.com/ATOS-VIRTUAL-CARE/apollo-ios", .upToNextMinor(from: "0.45.1")),
                    .package(url: "https://github.com/ATOS-VIRTUAL-CARE/webrtc-ios", .upToNextMinor(from: "90.0.0")),
                    ],
     targets: [
         .binaryTarget(
             name: "VcsRealtimeSdk",
-            url: "https://github.com/ATOS-VIRTUAL-CARE/realtime-sdk-ios/releases/download/0.5.5/VcsRealtimeSdk-0.5.5.xcframework.zip",
-            checksum: "5a2b9b0c9e680e0b44266b838ad787bc4282c431690df19acb0bd7ae4b8cf80f"
+            url: "https://github.com/ATOS-VIRTUAL-CARE/realtime-sdk-ios/releases/download/0.5.6/VcsRealtimeSdk-0.5.6.xcframework.zip",
+            checksum: "5d37c501acdeecddc7b23762a9b694e1fc7d03d500470b89ba4642149929af67"
         ),
     ]
 )
