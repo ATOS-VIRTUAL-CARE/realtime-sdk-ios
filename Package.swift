eeb8976f819db942cd77d763f640020ba5da53ce
Dana Brooks <dana.brooks@atos.net>
2021-10-25 09:07:38 -0400

    Update Package.swift for 0.7.4

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "VcsRealtimeSdk",
-            url: "https://github.com/ATOS-VIRTUAL-CARE/realtime-sdk-ios/releases/download/0.7.3/VcsRealtimeSdk-0.7.3.xcframework.zip",
-            checksum: "46a453edc282193ed9e17439464bb2369f76ccaee84543258af0e669cea11d5c"
+            url: "https://github.com/ATOS-VIRTUAL-CARE/realtime-sdk-ios/releases/download/0.7.4/VcsRealtimeSdk-0.7.4.xcframework.zip",
+            checksum: "d2b4d780236cdd4b7b1be3401834b1c4e4e048d5bb73dbe885325d28ab3fdd1e"
         ),
     ]
 )
