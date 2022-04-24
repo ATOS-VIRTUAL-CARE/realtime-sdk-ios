116d56d1313d4a92b379f31de329deba46b49688
Dana Brooks <5943844+danabrooks@users.noreply.github.com>
2022-04-24 13:52:56 -0700

    Update Package.swift for 0.9.0

--- Package.swift
+++ Package.swift
@@ -10,13 +10,13 @@ let package = Package(
             targets: ["VcsRealtimeSdk"]),
     ],
     dependencies: [ 
-                   .package(url: "https://github.com/ATOS-VIRTUAL-CARE/webrtc-ios", .upToNextMinor(from: "90.0.1")),
+                   .package(url: "https://github.com/ATOS-VIRTUAL-CARE/webrtc-ios", .upToNextMinor(from: "98.0.0")),
                    ],
     targets: [
         .binaryTarget(
             name: "VcsRealtimeSdk",
-            url: "https://github.com/ATOS-VIRTUAL-CARE/realtime-sdk-ios/releases/download/0.8.0/VcsRealtimeSdk-0.8.0.xcframework.zip",
-            checksum: "1de56d60a2129343922ccb78be9663f9a618274e052ec98f038f5620ac36534a"
+            url: "/realtime-sdk-ios/releases/download/0.9.0/VcsRealtimeSdk-0.9.0.xcframework.zip",
+            checksum: "197922a6d30f9f901c8a6ab121bbd20076187ee0a2c50676230c2263bd831d94"
         ),
     ]
 )
